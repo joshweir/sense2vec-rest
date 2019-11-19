99a0945b6c79fad2ae3b960a367cc46f257e9ade
joshweir <joshua.weir@gmail.com>
2019-11-19 18:14:17 +1100

    various changes

--- build-push-docker.sh
+++ build-push-docker.sh
@@ -10,4 +10,4 @@ docker push joshweir/sense2vec-rest:dev
 # docker run -it --rm -p "127.0.0.1:9188:80" joshweir/sense2vec-rest:dev
 
 # calling:
-# curl -H "Content-Type:text/plain" --data-binary "Penguins are birds, they are great." 'http://localhost:8080' | jq
+# curl -H "Content-Type:text/plain" --data-binary "[[\"plastic|NOUN\"]]" 'http://localhost:9188?min-scorez=0.76&nz=6&match-input-sensez=1&reduce-multi-wordformz=1&reduce-multicasez=1&min-word-len=2&reduce-compound-nouns=1'
